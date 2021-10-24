d1e96dded9f3b8b42d5c81724b38d2f7728f9fab
sw-develop <psw00719@naver.com>
2021-10-24 11:05:02 +0900

    [추가] 탈퇴를 위한 generic view

--- AppUser/views.py
+++ AppUser/views.py
@@ -14,3 +14,15 @@ class AddUserInfoView(UpdateAPIView):
 
     queryset = AppUser.objects.all()
     serializer_class = AddUserInfoSerializer
+
+
+class SignOutView(DestroyAPIView):
+    # 인증 & 허가 - JWTAuthentication, IsAuthenticated (기본 설정)
+
+    queryset = User.object.all()
+
+    def destroy(self, request, *args, **kwargs):
+        instance = User.objects.get(pk=request.user.id)
+        self.perform_destroy(instance)
+        content = {'탈퇴 완료'}
+        return Response(content, status=status.HTTP_204_NO_CONTENT)
