294add85bbdd0cadf47e987e5e151ba5ba9040ba
sw-develop <psw00719@naver.com>
2021-10-24 17:05:36 +0900

    [수정] 게시판 글 본문 TextField로 변경

--- Article/models.py
+++ Article/models.py
@@ -7,7 +7,7 @@ class Article(models.Model):
     id = models.BigAutoField(primary_key=True, db_column="article_id")
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="articles")
     title = models.CharField(max_length=32, null=False)
-    content = models.CharField(max_length=256, null=False)  # Text field로 해야되나..?
+    content = models.TextField(null=False)
     views = models.IntegerField(default=0)
     date_created = models.DateTimeField(auto_now=True)
     last_updated = models.DateTimeField(auto_now=True)
