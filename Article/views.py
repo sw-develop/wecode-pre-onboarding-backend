67cda711145e26d187000640c818dbe597b84075
sw-develop <psw00719@naver.com>
2021-10-26 14:32:59 +0900

    [삭제] 사용하지 않는 Permission import 제거

--- Article/views.py
+++ Article/views.py
@@ -1,5 +1,5 @@
 from rest_framework import viewsets, status
-from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 
 from Article.models import Article
